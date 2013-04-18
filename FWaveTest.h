85f6b35a47b61c8e912408636338063284b05f1a
mgutbrod@mytum.de <gutbrod@informatik.tu-muenchen.de>
2013-04-18 16:53:41 +0200

    Added the skeleton for the test class

---
+++ FWaveTest.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   FWaveTest.h
+ * Author: gutbrod
+ *
+ * Created on April 18, 2013, 4:52 PM
+ */
+
+#ifndef _FWAVETEST_H
+#define	_FWAVETEST_H
+
+#include <cxxtest/TestSuite.h>
+
+class FWaveTest : public CxxTest::TestSuite
+{
+public:
+    void testAddition(void)
+    {
+        
+    }
+};
+
+
+#endif	/* _FWAVETEST_H */
+
