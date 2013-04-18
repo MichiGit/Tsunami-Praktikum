e6c1b09c7481100cb2273a7f75326fc862a6cbc4
mgutbrod@mytum.de <gutbrod@informatik.tu-muenchen.de>
2013-04-18 16:33:55 +0200

    Implemented computeNetUpdates(...)

--- FWave.h
+++ FWave.h
@@ -55,5 +55,50 @@ template <class T> void FWave<T>::computeEigencoefficients(T hl, T hr, T hul, T 
     a2 = coefficient * (-c * deltaF1 + a * deltaF2);
 }
 
+template <class T> void FWave<T>::computeNetUpdates(T hl, T hr, T hul, T hur, T b1, T b2, T hNetUpdatesLeft, T hNetUpdatesRight, T huNetUpdatesLeft, T huNetUpdatesRight, T maxEdgeSpeed){
+    T fr1, fr2;
+    computeFluxFunction(hr, hur, fr1, fr2);
+    T fl1, fl2;
+    computeFluxFunction(hl, hul, fl1, fl2);
+    T deltaF1 = fr1 -fl1;
+    T deltaF2 = fr2 -fl2;
+    T a1, a2;
+    computeEigencoefficients(hl, hr, hul, hur, deltaF1, deltaF2, a1, a2);
+    T roe1, roe2;
+    computeRoeEigenvalues(hl, hr, hul, hur, roe1, roe2);
+    T z11 = a1 * roe1;
+    T z12 = a1 * roe2;
+    T z21 = a2 * roe1;
+    T z22 = a2 * roe2;
+    T ql1, ql2, qr1, qr2;
+    if (roe1 < 0){
+        ql1 += z11;
+        ql2 += z12;
+    } else if(roe1 >  0){
+        qr1 += z11;
+        qr2 += z12;
+    }
+    if (roe2 < 0){
+        ql1 += z21;
+        ql2 += z22;
+    } else if (roe2 >  0){
+        qr1 += z21;
+        qr2 += z22;
+    }
+    hNetUpdatesLeft = ql1;
+    hNetUpdatesRight = qr1;
+    huNetUpdatesLeft = ql2;
+    huNetUpdatesRight = qr2;
+    if(roe1 > 0 && roe2 > 0){
+        maxEdgeSpeed = roe2;
+    } else if(roe1 < 0 && roe2 < 0){
+        maxEdgeSpeed = 0;
+    } else if(roe1 >= roe2) {
+        maxEdgeSpeed = roe1;
+    } else {
+        maxEdgeSpeed = roe2;
+    }
+}
+
 #endif	/* _FWAVE_H */
 
