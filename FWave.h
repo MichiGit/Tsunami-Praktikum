7834a1e3845d59533256dc9ec8d379c69750ee9c
mgutbrod@mytum.de <gutbrod@informatik.tu-muenchen.de>
2013-04-18 13:11:10 +0200

    Implemented the first three functions

--- FWave.h
+++ FWave.h
@@ -7,8 +7,8 @@
 
 #ifndef _FWAVE_H
 #define	_FWAVE_H
-
-
+#include <math.h>
+#define g 9.81
 
 template <class T> class FWave {
 
@@ -20,10 +20,35 @@ public:
 
     T computeParticleVelocity(T hl, T hr, T hul, T hur);
 
-    void computeFluxFunction(T hl, T hr, T hul, T hur, T f1, T f2);
+    void computeFluxFunction(T h, T hu, T f1, T f2);
 
-    void computeEigencoefficients(T roe1, T roe2, T deltaF, T a1, T a2);
+    void computeEigencoefficients(T hl, T hr, T hul, T hur, T roe1, T roe2, T deltaF, T deltaF2, T a1, T a2);
 };
+
+template <class T> void FWave<T>::computeRoeEigenvalues(T hl, T hr, T hul, T hur, T roe1, T roe2) {
+    T pVelocity = computeParticleVelocity(hl, hr, hul, hur);
+    T height = 1.0 / 2.0 * (hl + hr);
+    T root = sqrt(g * height);
+    roe1 = pVelocity - root;
+    roe2 = pVelocity + root;
+}
+
+template <class T> void FWave<T>::computeFluxFunction(T h, T hu, T f1, T f2) {
+    f1 = hu;
+    f2 = pow(hu, 2) + 1.0 / 2.0 * g * pow(h, 2);
+}
+
+template <class T> void FWave<T>::computeEigencoefficients(T hl, T hr, T hul, T hur, T roe1, T roe2, T deltaF1, T deltaF2, T a1, T a2) {
+    T a = 1;
+    T b = 1;
+    T c, d;
+    computeRoeEigenvalues(hl, hr, hul, hur, c, d);
+    T coefficient = 1.0 / (d - c);
+    a1 = coefficient * (d * deltaF1 - b * deltaF2);
+    a2 = coefficient * (-c * deltaF1 + a * deltaF2);
+}
+
+
 
 #endif	/* _FWAVE_H */
 
