389d8d82cec434f702af64045507da9d6164edb3
dwsoto <david.soto@web.de>
2013-04-23 10:37:07 +0200

    implemented shock shock scenario, added types.h so that we can safely compile the scenario

---
+++ scenarios/shockshock.h
@@ -0,0 +1,45 @@
+#ifndef SCENARIOS_SHOCKSHOCK_H_
+#define SCENARIOS_SHOCKSHOCK_H_
+
+#include "../types.h"
+
+namespace scenarios
+{
+
+class ShockShock
+{
+private:
+    /** Number of cells */
+    const unsigned int m_size;
+
+public:
+    ShockShock(unsigned int size) : m_size(size) { }
+
+    /**
+     * @return Initial water height at pos
+     */
+    unsigned int getHeight(unsigned int pos)
+    {
+        return 300;
+    }
+
+    int getMomentum(unsigned int pos)
+    {
+        int v = 200;
+		if (pos <= m_size/2)
+			return v;
+		return -v;
+    }
+
+    /**
+     * @return Cell size of one cell (= domain size/number of cells)
+     */
+    T getCellSize()
+    {
+        return 1000.f / m_size;
+    }
+};
+
+}
+
+#endif
