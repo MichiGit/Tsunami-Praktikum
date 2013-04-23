ab92d5dbfe80aec03281b672a8a8d54ffb7c073a
dwsoto <david.soto@web.de>
2013-04-23 23:06:34 +0200

    added the extended version of the dambreak scenario

---
+++ scenarios/dambreak.h
@@ -0,0 +1,69 @@
+/**
+ * @file
+ *  This file is part of SWE1D
+ *
+ *  SWE1D is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  SWE1D is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with SWE1D.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *  Diese Datei ist Teil von SWE1D.
+ *
+ *  SWE1D ist Freie Software: Sie koennen es unter den Bedingungen
+ *  der GNU General Public License, wie von der Free Software Foundation,
+ *  Version 3 der Lizenz oder (nach Ihrer Option) jeder spaeteren
+ *  veroeffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ *  SWE1D wird in der Hoffnung, dass es nuetzlich sein wird, aber
+ *  OHNE JEDE GEWAEHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ *  Gewaehrleistung der MARKTFAEHIGKEIT oder EIGNUNG FUER EINEN BESTIMMTEN
+ *  ZWECK. Siehe die GNU General Public License fuer weitere Details.
+ *
+ *  Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ *  Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ *
+ * @copyright 2013 Technische Universitaet Muenchen
+ * @author Sebastian Rettenberger <rettenbs@in.tum.de>
+ */
+
+#ifndef SCENARIOS_DAMBREAK_H_
+#define SCENARIOS_DAMBREAK_H_
+
+#include "scenario.h"
+#include "../types.h"
+
+namespace scenarios
+{
+
+class DamBreak : Scenario
+{
+
+public:
+
+	unsigned int getHeight(unsigned int pos)
+	{
+		if (pos <= m_size/2)
+			return 253;
+
+		return 250;
+	}
+
+    int getMomentum(unsigned int pos)
+    {
+        if (pos <= m_size/2)
+            return 0;
+        return 20;
+    }
+};
+
+}
+
+#endif /* SCENARIOS_DAMBREAK_H_ */
